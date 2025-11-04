8120dee0709e587a168b15dec304d7740ca807e3
kevinbharris <68710571+kevinbharris@users.noreply.github.com>
2025-11-04 04:25:58 -0600

    Change layout extension for edit ticket view

--- src/Resources/views/admin/tickets/edit.blade.php
+++ src/Resources/views/admin/tickets/edit.blade.php
@@ -1,10 +1,10 @@
-@extends('admin::layouts.master')
+@extends('layouts.admin')
 
 @section('page_title')
     Edit Ticket
 @stop
 
-@section('content-wrapper')
+@section('content')
     <div class="content full-page">
         <div class="page-header">
             <div class="page-title">
