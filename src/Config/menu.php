51a1c3a0abd1c48235470602c0c390a3d34e55c6
kevinbharris <68710571+kevinbharris@users.noreply.github.com>
2025-11-05 02:24:37 -0600

    Refactor support menu structure in menu.php
    Reorganized the support menu structure for clarity and consistency.

--- src/Config/menu.php
+++ src/Config/menu.php
@@ -10,55 +10,47 @@ return [
     | The menu is registered in the admin sidebar.
     |
     */
-    'support' => [
-        'key'    => 'support',
-        'name'  => 'Help Desk',
-        'route'  => 'admin.support.tickets.index',
-        'icon'   => 'icon-support',
-        'sort'   => 10,
-        'children' => [
-            [
-                'key'   => 'support.canned_responses',
-                'name' => 'Canned Responses',
-                'route' => 'admin.support.canned-responses.index',
-                'icon'  => 'icon-canned-responses',
-                'sort'  => 1,
-            ],
-            [
-                'key'   => 'support.categories',
-                'name' => 'Categories',
-                'route' => 'admin.support.categories.index',
-                'icon'  => 'icon-categories',
-                'sort'  => 2,
-            ],
-            [
-                'key'   => 'support.priorities',
-                'name' => 'Priorities',
-                'route' => 'admin.support.priorities.index',
-                'icon'  => 'icon-priorities',
-                'sort'  => 3,
-            ],
-            [
-                'key'   => 'support.rules',
-                'name' => 'Rules',
-                'route' => 'admin.support.rules.index',
-                'icon'  => 'icon-rules',
-                'sort'  => 4,
-            ],
-            [
-                'key'   => 'support.statuses',
-                'name' => 'Statuses',
-                'route' => 'admin.support.statuses.index',
-                'icon'  => 'icon-statuses',
-                'sort'  => 5,
-            ],
-            [
-                'key'   => 'support.tickets',
-                'name' => 'Tickets',
-                'route' => 'admin.support.tickets.index',
-                'icon'  => 'icon-ticket',
-                'sort'  => 6,
-            ],
-        ]
-    ],
+		[ 
+			'key'    => 'support',
+			'name'  => 'Help Desk',
+			'route'  => 'admin.support.tickets.index',
+			'sort'   => 4,
+			'icon'   => 'icon-support',
+		],[
+            'key'   => 'support.canned_responses',
+            'name' => 'Canned Responses',
+            'route' => 'admin.support.canned-responses.index',
+            'icon'  => 'icon-canned-responses',
+            'sort'  => 1,
+        ],[
+            'key'   => 'support.categories',
+            'name' => 'Categories',
+            'route' => 'admin.support.categories.index',
+            'icon'  => 'icon-categories',
+            'sort'  => 2,
+        ],[
+            'key'   => 'support.priorities',
+            'name' => 'Priorities',
+            'route' => 'admin.support.priorities.index',
+            'icon'  => 'icon-priorities',
+            'sort'  => 3,
+        ],[
+            'key'   => 'support.rules',
+            'name' => 'Rules',
+            'route' => 'admin.support.rules.index',
+            'icon'  => 'icon-rules',
+            'sort'  => 4,
+        ],[
+            'key'   => 'support.statuses',
+            'name' => 'Statuses',
+            'route' => 'admin.support.statuses.index',
+            'icon'  => 'icon-statuses',
+            'sort'  => 5,
+        ],[
+            'key'   => 'support.tickets',
+            'name' => 'Tickets',
+            'route' => 'admin.support.tickets.index',
+            'icon'  => 'icon-ticket',
+            'sort'  => 6,
+        ],
 ];
